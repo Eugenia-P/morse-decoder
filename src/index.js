6261071034466c38eda09bfba6f822dfaac767a2
Eugenia-P <eugenia.protasevich@yandex.by>
2019-10-12 13:56:30 +0300

    feat: my solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,30 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let coded = []; 
+    for (let i = 0; i < expr.length; i+=10) {
+        coded.push(expr.substring(i, i+10)); 
+    }
+    let decoded = [];
+   
+    coded.forEach(el => {
+        
+        el = (el === '**********') ? ' ' : 
+                                   String(parseInt(el, 10));
+  
+        for (i = 0; i < el.length; i+=2) {
+
+            el = (el.indexOf('10') !== -1) ? el.split('10').join('.') : 
+                                             el.split('11').join('-');
+        }
+        
+        if (el !== ' ') {
+            decoded.push(MORSE_TABLE[el]);
+        } else decoded.push(el);
+   
+    });
+    
+    return decoded.join(''); 
 }
 
 module.exports = {
